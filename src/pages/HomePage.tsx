66a9aecda6c954a7aead4322fa631102678fe327
Anas Obaid <anasobaidpk@gmail.com>
2024-07-25 12:00:54 +0500

    Build the projects section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Hero from "../components/Hero";
+import Work from "../components/Work";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <About />
+      <Work />
     </>
   );
 };
