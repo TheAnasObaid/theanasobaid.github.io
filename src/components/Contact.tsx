6882d5459c9b75dc1b1bd5a305f71f40f3832461
Anas Obaid <anasobaidpk@gmail.com>
2024-07-25 13:51:22 +0500

    Build the footer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,18 @@
+import Footer from "./Footer";
+
 const Contact = () => {
   return (
-    <div className="flex flex-col w-full justify-center gap-5 items-center py-36 md:py-56">
-      <p className="text-xl md:text-3xl text-neutral-400 text-center">
-        Have a project in mind?
-      </p>
-      <h2 className="text-6xl md:text-9xl text-neutral-100 hover:border-b-2 text-center transition-all duration-100">
-        <a href="mailto:anasobaidpk@gmail.com">Contact me</a>
-      </h2>
-    </div>
+    <>
+      <div className="flex flex-col w-full justify-center gap-5 items-center py-36 md:py-56">
+        <p className="text-xl md:text-3xl text-neutral-400 text-center">
+          Have a project in mind?
+        </p>
+        <h2 className="text-6xl md:text-9xl text-neutral-100 hover:border-b-2 text-center transition-all duration-100">
+          <a href="mailto:anasobaidpk@gmail.com">Contact me</a>
+        </h2>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className="text-neutral-100 mx-auto flex justify-between items-center max-w-screen-xl pb-10 px-5 md:px-0">
+      <span className="text-lg text-neutral-400">&copy; Copyright 2024</span>
+      <a
+        className="text-lg text-neutral-400 border-b-2 border-neutral-400 hover:text-neutral-200 hover:border-neutral-200 transition-all duration-100"
+        href="#"
+      >
+        Linkedin
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
